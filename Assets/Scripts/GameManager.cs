8e6108041b1e997b8290a4632ca9e178f4b3bcee
danialaa <d.a.mahdaly@outlook.com>
2025-05-30 01:04:54 +0300

    Trigger collision and activate second state and switch runner

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,10 +15,21 @@ public class GameManager : MonoBehaviour
     private List<PlayerController> characters = new List<PlayerController>();
     private PlayerController currentPlayer;
 
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+            Instance = this;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         CreateFirstPlayer();
+        CreateIdleCharacters();
 
         //float minX = plane.position.x - (plane.localScale.x / 2) + playerPrefab.transform.localScale.x;
         //float maxX = plane.position.x + (plane.localScale.x / 2) - playerPrefab.transform.localScale.x;
@@ -31,12 +42,32 @@ public class GameManager : MonoBehaviour
         GameObject newCharacter = Instantiate(playerPrefab, playerPrefab.transform.position, Quaternion.identity);
         currentPlayer = newCharacter.GetComponent<PlayerController>();
         currentPlayer.StartRunning();
+
+        characters.Add(currentPlayer);
     }
 
+    private void CreateIdleCharacters()
+    {
+        int[] pos = new int[3] { -10, -4, 4 };
+
+        for (int i = 0; i < idleCt; i++)
+        {
+            //Vector3 randomPos = new Vector3(UnityEngine.Random.Range(minX, maxX), playerPrefab.transform.position.y, UnityEngine.Random.Range(minZ, maxZ));
+            GameObject newCharacter = Instantiate(playerPrefab, /*randomPos*/ new Vector3(playerPrefab.transform.position.x, playerPrefab.transform.position.y, pos[i]), Quaternion.identity);
+            newCharacter.GetComponentInChildren<Renderer>().material = colorPool[UnityEngine.Random.Range(0, colorPool.Count)];
+
+            characters.Add(newCharacter.GetComponent<PlayerController>());
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetNewRunner(GameObject newRunner)
+    {
+        currentPlayer = newRunner.GetComponent<PlayerController>();
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     bool isRunning = false;
+    bool isStacked = false;
+    float yAmountToElevate = 1f;
+    float zAmountToSit = -1f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,4 +26,30 @@ public class PlayerController : MonoBehaviour
         gameObject.GetComponent<Animator>().SetBool("isRunning", true);
     }
 
+    public void StackCharacter()
+    {
+        isRunning = false;
+        isStacked = true;
+        gameObject.GetComponent<Animator>().SetBool("isRunning", false);
+        gameObject.GetComponent<Animator>().SetBool("isStacked", true);
+
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+        transform.position = new Vector3(transform.position.x, transform.position.y + yAmountToElevate, transform.position.z + zAmountToSit);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Character")
+        {
+            if (!isRunning)
+            {
+                StartRunning();
+                GameManager.Instance.SetNewRunner(gameObject);
+            }
+            else
+            {
+                StackCharacter();
+            }
+        }
+    }
 }
